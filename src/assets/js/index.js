92382def0eb18345c49a535e1abe9de6a8356746
Rasuli Azamat <rasuliazamat@gmail.com>
2022-06-10 02:24:06 +0500

    Fix scroll bug

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -3,9 +3,15 @@ let mobileMenu = null
 setInterval(() => {
   if (mobileMenu) {
     if (mobileMenu.classList.contains('visible')) {
-      return (document.body.style.overflow = 'hidden')
+      return (
+        (document.documentElement.style.touchAction = 'none'),
+        (document.documentElement.style.overflow = 'hidden')
+      )
     }
-    return (document.body.style.overflow = 'auto')
+    return (
+      (document.documentElement.style.touchAction = 'auto'),
+      (document.documentElement.style.overflow = 'auto')
+    )
   }
-  return mobileMenu = document.querySelector('.mobile-menu')
+  return (mobileMenu = document.querySelector('.mobile-menu'))
 })
